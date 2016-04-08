019ee76cc17bbac27d275a15c97e36a697e24bf8
Walter Barbagallo <walter.barbagallo@h-farm.com>
2016-04-08 18:30:20 +0200

    testing .root fluency

--- test/guidanceTest.js
+++ test/guidanceTest.js
@@ -130,12 +130,13 @@ describe('guidance', function() {
     it('use a fluent interface', function() {
       let routes = function(router) {
         router
-          .get('/hp', { to: 'welcome#homepage', as: 'homepage' })
-          .post('/photos', { to: 'photos#create' })
+          .get('/geocoder', { to: 'geocoder#show' })
           .namespace('admin', function() {})
           .resources('books')
           .resource('geocoder')
           .scope('admin', function() {})
+          .root('welcome#index')
+          .post('/photos', { to: 'photos#create' })
         ;
       };
 
